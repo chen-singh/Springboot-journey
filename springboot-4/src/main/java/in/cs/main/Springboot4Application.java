5525ee691a0f53f084d46a608e0845b5f2cb8368
chen-singh <cssodho123@gmail.com>
2026-05-16 11:23:45 +0400

    add updates

--- springboot-4/src/main/java/in/cs/main/Springboot4Application.java
+++ springboot-4/src/main/java/in/cs/main/Springboot4Application.java
@@ -1,13 +1,27 @@
 package in.cs.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import in.cs.main.beans.Student;
 
 @SpringBootApplication
-public class Springboot4Application {
+public class Springboot4Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot4Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		stdBean().display();
+	}
+	@Bean
+	public Student stdBean() {
+		return new Student("chensingh",8,56.6f);
 	}
 
 }
--- springboot-4/src/main/java/in/cs/main/beans/Student.java
+++ springboot-4/src/main/java/in/cs/main/beans/Student.java
@@ -2,4 +2,38 @@ package in.cs.main.beans;
 
 public class Student {
 
+	private String name;
+	private int rolno;
+	private float marks;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRolno() {
+		return rolno;
+	}
+	public void setRolno(int rolno) {
+		this.rolno = rolno;
+	}
+	public float getMarks() {
+		return marks;
+	}
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	public Student(String name, int rolno, float marks) {
+		super();
+		this.name = name;
+		this.rolno = rolno;
+		this.marks = marks;
+	}
+	
+	
+	
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.print("name "+name+"rolno"+rolno+"marks : "+marks);
+	}
 }
