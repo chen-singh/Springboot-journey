47e1c292293816e762cf92a42544a85c149033fd
chen-singh <cssodho123@gmail.com>
2026-05-16 11:53:18 +0400

    add updates

--- springboot-3/src/main/java/in/cs/main/Myclass.java
+++ springboot-3/src/main/java/in/cs/main/Myclass.java
@@ -1,6 +1,8 @@
 package in.cs.main;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class Myclass {
 
 	public void printMsg(String name) {
--- springboot-3/src/main/java/in/cs/main/Springboot3Application.java
+++ springboot-3/src/main/java/in/cs/main/Springboot3Application.java
@@ -1,27 +1,29 @@
 package in.cs.main;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Springboot3Application  implements CommandLineRunner{
+public class Springboot3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Springboot3Application.class, args);
+		ApplicationContext context=SpringApplication.run(Springboot3Application.class, args);
+       Myclass m=  context.getBean(Myclass.class);
+	  m.printMsg("Rana Chen Singh");
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		myclas().printMsg("chen");
-	}
-	@Bean
-	public Myclass myclas() {
-		
-		return new Myclass();
-		
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		// TODO Auto-generated method stub
+//		myclas().printMsg("chen");
+//	}
+//	@Bean
+//	public Myclass myclas() {
+//		
+//		return new Myclass();
+//		
+//	}
 
 }
