f287a7b6e1bee6bef6caead54476f9111bcc07f3
chen-singh <cssodho123@gmail.com>
2026-05-29 01:34:52 +0500

    user entity

--- springbootsecurity/src/main/java/in/cs/main/entities/USers.java
+++ springbootsecurity/src/main/java/in/cs/main/entities/USers.java
@@ -1,13 +1,17 @@
 package in.cs.main.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table
 public class USers {
 	
-	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
