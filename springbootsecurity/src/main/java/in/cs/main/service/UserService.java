3aec81eaf0fd262ca987666212d6c3b3fb63c4c4
chen-singh <cssodho123@gmail.com>
2026-05-29 01:24:45 +0500

    user service layer

--- springbootsecurity/src/main/java/in/cs/main/service/UserService.java
+++ springbootsecurity/src/main/java/in/cs/main/service/UserService.java
@@ -1,5 +1,19 @@
 package in.cs.main.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import in.cs.main.entities.USers;
+import in.cs.main.repository.UserRepository;
+
+@Service
 public class UserService {
 
+	@Autowired
+	private UserRepository userRepo;
+	
+	public USers createUser(USers user) {
+		return userRepo.save(user);
+		
+	}
 }
