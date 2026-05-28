8a2fff81b453881cc8329c08edab3c2fab1ca759
chen-singh <cssodho123@gmail.com>
2026-05-29 01:29:00 +0500

    controller layer

--- springbootsecurity/src/main/java/in/cs/main/controller/AuthController.java
+++ springbootsecurity/src/main/java/in/cs/main/controller/AuthController.java
@@ -1,5 +1,23 @@
 package in.cs.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.cs.main.entities.USers;
+import in.cs.main.service.UserService;
+
+@RestController
 public class AuthController {
+	
+	@Autowired
+	private UserService service;
+	
+	@PostMapping("/register")
+	public USers register(@RequestBody USers user) {
+		return service.createUser(user);
+		
+	}
 
 }
