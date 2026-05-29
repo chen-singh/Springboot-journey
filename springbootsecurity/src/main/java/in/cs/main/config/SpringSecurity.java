d9c03e4356c356f6b58a7d112fdf8fc20f9a3eef
chen-singh <cssodho123@gmail.com>
2026-05-30 00:46:58 +0500

    updates security class

--- springbootsecurity/src/main/java/in/cs/main/config/SpringSecurity.java
+++ springbootsecurity/src/main/java/in/cs/main/config/SpringSecurity.java
@@ -1,14 +1,23 @@
 package in.cs.main.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+import in.cs.main.service.UserDetailServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity {
+	
+	private static final String DaoAuthenticationProvider = null;
+	@Autowired
+	UserDetailServiceImpl userdetails;
 	
 	@Bean
 	public SecurityFilterChain securitychain(HttpSecurity http) {
@@ -20,6 +29,18 @@ public class SpringSecurity {
 		 
 		 return http.build();
 		
+	}
+	
+	@Bean
+	public AuthenticationProvider authProvider() {
+
+	    DaoAuthenticationProvider authProvider =
+	            new DaoAuthenticationProvider();
+
+	    authProvider.setUserDetailsService(userdetails);
+	    authProvider.setPasswordEncoder(passwordEncoder());
+
+	    return authProvider;
 	}
 
 }
