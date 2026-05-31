40f3894d8f69ba2d7cf1dd1d48b5755e96a0c8b6
chen-singh <cssodho123@gmail.com>
2026-05-31 22:22:20 +0500

    add files

--- springbootsecurity/src/main/java/in/cs/main/service/UserDetailServiceImpl.java
+++ springbootsecurity/src/main/java/in/cs/main/service/UserDetailServiceImpl.java
@@ -1,5 +1,36 @@
 package in.cs.main.service;
 
-public class UserDetailServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import in.cs.main.entities.USers;
+import in.cs.main.repository.UserRepository;
+
+@Component
+public class UserDetailServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		USers user=repo.findByUsername(username);
+		if(user!=null) {
+		
+		return	User.builder()
+			.username(user.getUsername())
+			.password(user.getPassword())
+			.roles(user.getRoles().toArray(new String[0]))
+			.build();
+			 
+		}
+		throw new UsernameNotFoundException("User not found"+username);
+		
+	}
 
 }
