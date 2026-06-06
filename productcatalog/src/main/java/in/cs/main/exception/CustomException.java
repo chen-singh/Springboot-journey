9ed1e519fac38bda5006bb595988d9468bd63769
chen-singh <cssodho123@gmail.com>
2026-06-06 21:43:48 +0400

    add files

--- productcatalog/src/main/java/in/cs/main/exception/CustomException.java
+++
@@ -1,5 +0,0 @@
-package in.cs.main.exception;
-
-public class CustomException {
-
-}
---
+++ productcatalog/src/main/java/in/cs/main/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package in.cs.main.exception;
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
