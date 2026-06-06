3beb519cf682ccd9f16047ba05d537fc2c97e9d2
chen-singh <cssodho123@gmail.com>
2026-06-06 21:29:40 +0400

    add files

--- productcatalog/src/main/java/in/cs/main/ProductcatalogApplication.java
+++ productcatalog/src/main/java/in/cs/main/ProductcatalogApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootsecurityApplication {
+public class ProductcatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootsecurityApplication.class, args);
-		
+		SpringApplication.run(ProductcatalogApplication.class, args);
 	}
 
 }
---
+++ productcatalog/src/main/java/in/cs/main/entities/Product.java
@@ -0,0 +1,5 @@
+package in.cs.main.entities;
+
+public class Product {
+
+}
